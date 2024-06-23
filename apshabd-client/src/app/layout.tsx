65893f8d51fb9177e77d3f0617364ed6e70b854b
Sarvagya Kumar <111130903+sarvagyakrcs@users.noreply.github.com>
2024-06-23 08:02:01 +0530

    Update layout.tsx

--- apshabd-client/src/app/layout.tsx
+++ apshabd-client/src/app/layout.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 const font = Open_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "Apshabd",
+    title: "Apshabd | अपशब्द ",
     description: "The Profanity Detection API is a web service designed to detect and flag profane language in text messages. Built using the Hono framework and deployed on a serverless environment such as Cloudflare Workers, this API leverages vector search technologies to accurately identify offensive language.",
 };
 
